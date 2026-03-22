018b4db89f3fc2fc5984a0ce1399cbc38513057e
khaled mohamed <khaleedmo05@gmail.com>
2026-03-23 00:35:31 +0200

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales_Database.Sales_Database.models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales_Database.ApplicationDbContext
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.; Initial Catalog=EfProject2; Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;");
+        }
+    }
+}
